ba38b1d68e88d8611b58e192eed4f7dfe8974cd3
Sean Park <seansangpark@gmail.com>
2021-11-24 19:13:09 -0800

    Custom middleware created for auth get request and jwt verification

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,7 +11,6 @@ const User = require('../../models/User');
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
-
 router.post(
   '/',
   // custom validation using express validator
