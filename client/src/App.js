36caf1a51b3b03fea45e501d55bccbd59dffca75
Sean Park <seansangpark@gmail.com>
2021-11-26 12:58:56 -0800

    Utilized react-router-dom for router setup to register and login components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 import './App.css';
 
 const App = () => (
-  <>
+  <Router>
     <Navbar />
-    <Landing />
-  </>
+    <Routes>
+      <Route exact path='/' element={<Landing />} />
+    </Routes>
+    <section className='container'>
+      <Routes>
+        <Route exact path='/register' element={<Register />} />
+        <Route exact path='/login' element={<Login />} />
+      </Routes>
+    </section>
+  </Router>
 );
 
 export default App;
