88f1b8e815f7c01b7d86fad9e8b77d3041d9120b
Sean Park <seansangpark@gmail.com>
2021-11-26 16:17:00 -0800

    Redux setup complete for alerts using actions and reducers

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,30 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Navbar />
-    <Routes>
-      <Route exact path='/' element={<Landing />} />
-    </Routes>
-    <section className='container'>
+  <Provider store={store}>
+    <Router>
+      <Navbar />
       <Routes>
-        <Route exact path='/register' element={<Register />} />
-        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/' element={<Landing />} />
       </Routes>
-    </section>
-  </Router>
+      <section className='container'>
+        <Alert />
+        <Routes>
+          <Route exact path='/register' element={<Register />} />
+          <Route exact path='/login' element={<Login />} />
+        </Routes>
+      </section>
+    </Router>
+  </Provider>
 );
 
 export default App;
