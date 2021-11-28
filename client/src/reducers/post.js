7c1dbcd133e6eba41403c589a009f04c9ada2c64
Sean Park <seansangpark@gmail.com>
2021-11-27 22:46:49 -0800

    Post input box functionality implemented and saving text in db and re-rendering posts

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -4,6 +4,7 @@ import {
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
+  ADD_POST,
 } from '../actions/types.js';
 
 const initialState = {
@@ -35,6 +36,12 @@ export default function (state = initialState, action) {
         posts: state.posts.map((post) =>
           post._id === payload.postId ? { ...post, likes: payload.likes } : post
         ),
+        loading: false,
+      };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [payload, ...state.posts],
         loading: false,
       };
     case DELETE_POST:
