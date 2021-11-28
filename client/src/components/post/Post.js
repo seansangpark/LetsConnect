6c948fde5c0a2c0678fea0126825390bc122824c
Sean Park <seansangpark@gmail.com>
2021-11-27 23:31:35 -0800

    Comment form created and posting text to db

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
+import CommentForm from './CommentForm';
 import { getPost } from '../../actions/post';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -17,10 +18,12 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
     <Spinner />
   ) : (
     <div className='container'>
+      {console.log(post._id)}
       <Link to='/posts' className='btn'>
         Back To Posts
       </Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
     </div>
   );
 };
