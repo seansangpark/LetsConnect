b565aa9f6a38a9a4c553f8469db2640eeb373abb
Sean Park <seansangpark@gmail.com>
2021-11-28 12:31:57 -0800

    NotFound component created. Project complete

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
 import Post from './components/post/Post';
+import NotFound from './components/layout/NotFound';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 // Redux
@@ -70,6 +71,7 @@ const App = () => {
             path='/posts/:id'
             element={<PrivateRoute component={Post} />}
           />
+          <Route element={<NotFound />} />
         </Routes>
       </Router>
     </Provider>
